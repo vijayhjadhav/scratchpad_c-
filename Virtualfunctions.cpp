203175018479d1f09d2d52c8e630a742db69f675
vijayhjadhav <vijayhjadhav@gmail.com>
2020-09-05 10:31:08 +1000

    Update Virtualfunctions.cpp

--- Virtualfunctions.cpp
+++ Virtualfunctions.cpp
@@ -1,108 +1,41 @@
-#include<iostream>
-#include<string>
-#include<vector>
-using namespace std;
+#include <iostream>  
+using namespace std;  
+  
+class Account {  
+public:  
+   Account( double d ) { _balance = d; }  
+   virtual double GetBalance() { return _balance; }  
+   virtual void PrintBalance() { cerr << "Error. Balance not available for base type." << endl; }  
+private:  
+    double _balance;  
+};  
+  
+class CheckingAccount : public Account {  
+public:  
+   CheckingAccount(double d) : Account(d) {}  
+   void PrintBalance() { cout << "Checking account balance: " << GetBalance() << endl; }  
+};  
+  
+class SavingsAccount : public Account {  
+public:  
+   SavingsAccount(double d) : Account(d) {}  
+   void PrintBalance() { cout << "Savings account balance: " << GetBalance(); }  
+};  
+  
+int main() {  
+   // Create objects of type CheckingAccount and SavingsAccount.  
+   CheckingAccount *pChecking = new CheckingAccount( 100.00 ) ;  
+   SavingsAccount  *pSavings  = new SavingsAccount( 1000.00 );  
+  
+   // Call PrintBalance using a pointer to Account.  
+   Account *pAccount = pChecking;  
+   pAccount->PrintBalance();
+   pAccount->Account::PrintBalance();  // Virtual function mechanism is suppressed here. Call the base class method.
+   pChecking->Account::PrintBalance(); // Virtual function mechanism is suppressed here. Call the base class method.
+  
+   // Call PrintBalance using a pointer to Account.  
+   pAccount = pSavings;  
+   pAccount->PrintBalance();
 
-class Person
-    {
-    public:
-	Person(){};	
-    Person(string name, int age):m_name(name), m_age(age)
-    {        
-    }
-	virtual void getdata() = 0;
-	virtual void putdata() = 0;
-    protected:
-    string m_name;
-    int m_age;    
-};
-
-class Professor : public Person
-    {
-    public:
-	Professor(){m_currID = ++m_SequenceID;}	
-    Professor(string name, int age, int publications):Person(name, age), m_publications(publications)
-    {     
-    }
-	void getdata();
-	void putdata();
-    private:
-    int m_publications;
-	 int m_currID;
-	static int m_SequenceID;
-};
-
-int Professor::m_SequenceID = 0;
-
-void Professor::getdata()
-{
-	cin >> m_name >> m_age >> m_publications; 
-}
-
-void Professor::putdata()
-{
-	cout << m_name << " " << m_age << " " << m_publications << " " << m_currID << endl;
-}
-
-class Student : public Person
-    {
-    public:
-	Student(){m_currID = ++m_SequenceID;}	
-    Student(string name, int age, int *marks): Person(name, age), m_marks(marks)
-    {        
-    }
-	void getdata();
-	void putdata();
-    private:
-    int *m_marks;
-	int m_currID;
-	static int m_SequenceID;
-};
-
-int Student::m_SequenceID = 0;
-
-void Student::getdata()
-{
-	cin >> m_name >> m_age;
-	m_marks = new int[6];
-	for(int i = 0; i < 6; i++)
-		cin >> m_marks[i];
-}
-
-void Student::putdata()
-{
-	int sum = 0;
-	for(int i = 0; i < 6; i++)
-		sum = sum + m_marks[i];
-	cout << m_name << " " << m_age << " " << sum << " " << m_currID << endl; 	
-}
-
-int main()
-{
-	int numInputs;
-	int personType;
-	cin >> numInputs;
-	
-	vector<Person *> ListPersons;
-	Person *ptrPerson = NULL;
-	for(int i = 0; i < numInputs; i++)
-	{
-		cin >> personType;
-		if(personType == 1)   // Professor
-		{
-			ptrPerson = new Professor();
-		}
-		else  // Student
-		{
-			ptrPerson = new Student();
-		}
-		ptrPerson->getdata();
-		ListPersons.push_back(ptrPerson);
-	}
-
-	for(int i = 0; i < numInputs; i++)
-	{
-		ListPersons[i]->putdata();
-	}
-	return 0;
-}
+   return 0;
+}  
