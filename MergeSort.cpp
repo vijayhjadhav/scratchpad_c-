b5ca3e61baf4037c173f5e328db7cea0dbeb74f5
vijayhjadhav <vijayhjadhav@gmail.com>
2020-10-15 23:02:14 +1100

    Updated code to count the inversions

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-void merge(vector<int>& vArray, int left, int middle, int right);
+int merge(vector<int>& vArray, int left, int middle, int right);
+int mgSort(vector<int>& vArray, int left, int right);
 
-void mgSort(vector<int> &vArray, int left, int right)
+int mgSort(vector<int> &vArray, int left, int right)
 {
+	int iInvCounter = 0;
 	if (left < right)
 	{
 		int middle = (left+right) / 2;
-		mgSort(vArray, left, middle);
-		mgSort(vArray, middle + 1, right);
-		merge(vArray, left, middle, right);
+		iInvCounter += mgSort(vArray, left, middle);
+		iInvCounter += mgSort(vArray, middle + 1, right);
+		iInvCounter += merge(vArray, left, middle, right);
 	}
+	return iInvCounter;
 }
 
-void merge(vector<int>& vArray, int left, int middle, int right)
+int merge(vector<int>& vArray, int left, int middle, int right)
 {
 	vector<int> vTempArray;
+	int iInvCounter = 0;
 	int i = left;
 	int j = middle + 1;
 	while (i <= middle && j <= right)
@@ -30,6 +34,8 @@ void merge(vector<int>& vArray, int left, int middle, int right)
 		{
 			vTempArray.push_back(vArray[j]);
 			j++;
+			// count the inversions
+			iInvCounter += middle-i+1;
 		}
 	}
 
@@ -50,12 +56,13 @@ void merge(vector<int>& vArray, int left, int middle, int right)
 	{
 		vArray[i] = vTempArray[i-left];
 	}
+	return iInvCounter;
 }
 
 void printArray(vector<int> vArray)
 {
 	size_t sz = vArray.size();
-	for (int i = 0; i < sz; i++)
+	for (size_t i = 0; i < sz; i++)
 	{
 		cout << vArray[i] << "\t";
 	}
@@ -65,9 +72,12 @@ void printArray(vector<int> vArray)
 
 int main()
 {
-	vector<int> vArray{ 23,45,24,5,7,9,10,15,20 };
+	//vector<int> vArray{ 23,45,24,5,7,9,10,15,20 };
+	//vector<int> vArray{ 1, 3, 5, 2, 4, 6 };
+	//vector<int> vArray{ 50, 25, 30, 2, 56, 5, 4, 1 };
+	vector<int> vArray{ 23, 4, 6, 9, 2, 1, 10 };
 	printArray(vArray);
-	mgSort(vArray, 0, vArray.size() - 1);
+	int iInvCount = mgSort(vArray, 0, vArray.size() - 1);
 	printArray(vArray);
 	return 0;
 }
